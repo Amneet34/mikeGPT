248416de5041c4307c2f845944542d9f5a994c11
Zaid Almuflahi <Zaid.almuflahi1217@gmail.com>
2023-01-24 11:07:48 -0500

    sleepy time

--- backend/seeds.py
+++ backend/seeds.py
@@ -1,5 +1,5 @@
 from app import app
-from models import db, User, Question, Answer
+from models import db, User, Answer
 
 def run_seeds():
     print('Seeding database ... 🌱')
@@ -8,17 +8,9 @@ def run_seeds():
       db.session.add(user)
       db.session.commit()
       user = User.query.first()
-      seeded_questions = []
-      for _ in range(1):
-        question = Question('should we trust the system?')
-        question.user_id = user.id
-        seeded_questions.append(question)
-      db.session.add_all(seeded_questions)
-      db.session.commit()
       seeded_answers = []
       for _ in range(1):
         answer = Answer('Nah fuck the system')
-        answer.question_id = question.id
         seeded_answers.append(answer)
       db.session.add_all(seeded_answers)
       db.session.commit()
